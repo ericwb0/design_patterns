a36d8ec0279a07e7a7476216781c764b4e1641ae
ericwb0 <burkholdereric03@gmail.com>
2022-10-01 19:46:51 -0400

    Javadoc, bugfixes, output screenshot

--- singleton/ClothingItem.java
+++ singleton/ClothingItem.java
@@ -13,22 +13,23 @@ public class ClothingItem {
     private ArrayList<Season> seasons;
 
     /**
-     * 
-     * @param name
-     * @param part
-     * @param color
+     * Constructor instantiates ClothingItem with an empty seasons ArrayList
+     * @param name name of the clothing
+     * @param part what part the outfit is ie. TOP, BOTTOM, or WHOLE
+     * @param color color as a String
      */
     public ClothingItem(String name, ClothingPart part, String color) {
-        this.name = name;
+        // Trim is to get rid of any unwanted whitespace
+        this.name = name.trim();
         this.part = part;
-        this.color = color;
+        this.color = color.trim();
         seasons = new ArrayList<Season>();
     }
 
     /**
-     * 
-     * @param season
-     * @return
+     * Checks if the given season is in the seasons ArrayList
+     * @param season season enum to search for
+     * @return true if the season is in the ClothingItem's seasons ArrayList, false if not
      */
     public boolean hasSeason(Season season) {
         for(Season s : seasons)
@@ -37,15 +38,15 @@ public class ClothingItem {
     }
 
     /**
-     * 
-     * @param season
+     * Adds the given season to the seasons ArrayList
+     * @param season season enum to add
      */
     public void addSeason(Season season) {
         seasons.add(season);
     }
 
     /**
-     * 
+     * Returns a String representation of the item
      */
     @Override
     public String toString() {
--- singleton/OutfitCreator.java
+++ singleton/OutfitCreator.java
@@ -3,6 +3,9 @@ package singleton;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * OutfitCreator singleton class
+ */
 public class OutfitCreator {
     private ArrayList<ClothingItem> tops;
     private ArrayList<ClothingItem> bottoms;
@@ -11,7 +14,8 @@ public class OutfitCreator {
     private static OutfitCreator outfitCreator;
 
     /**
-     * 
+     * Constructor. Reads the tops, bottoms, and wholes files to populate the respective ArrayLists
+     * with ClothingItems
      */
     private OutfitCreator() {
         tops = FileReader.getClothing("singleton/txt/tops.txt", ClothingPart.TOP);
@@ -21,8 +25,8 @@ public class OutfitCreator {
     }
 
     /**
-     * 
-     * @return
+     * Returns the single instance of OutfitCreator. On the first call of this method it will
+     * instantiate the OutfitCreator
      */
     public static OutfitCreator getInstance() {
         if(outfitCreator == null)
@@ -31,37 +35,37 @@ public class OutfitCreator {
     }
 
     /**
-     * 
-     * @param season
-     * @return
+     * Creates a randomly generated outfit for the specified season. 50% chance to get a one-piece
+     * outfit, 50% to get a top/bottom combination
+     * @param season season enum to find outfit for
+     * @return a String representing the outfit
      */
     public String getOutfit(Season season) {
         int choice = rand.nextInt(2); // Bound 2 to produce 0 or 1
-        boolean picked = false;
         // 50% chance to get whole outfit
         if(choice == 0) {
             ClothingItem whole;
-            while(!picked) {
+            // These loops ensure that when a piece is picked, it fits the season
+            while(true) {
                 whole = wholes.get(rand.nextInt(wholes.size()));
+                // Break when the ClothingItem fits
                 if(whole.hasSeason(season))
                     return "A " + whole;
             }
         } else { // Top/bottom combo
             ClothingItem top = null;
             ClothingItem bottom = null;
-            while(!picked) {
+            while(true) {
                 top = tops.get(rand.nextInt(tops.size()));
                 if(top.hasSeason(season))
-                    picked = true;
+                    break;
             }
-            picked = false;
-            while(!picked) {
+            while(true) {
                 bottom = bottoms.get(rand.nextInt(bottoms.size()));
                 if(bottom.hasSeason(season))
-                    picked = true;
+                    break;
             }
             return "A " + top + " and " + bottom;
         }
-        return null;
     }
 }
