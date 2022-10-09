51e2588d5651bce4d96a15fe075880d65deb9046
SavannahNoblitt <savannahmanob@gmail.com>
2022-10-09 19:42:24 -0400

    WriteCommand Class

--- src/WriteCommand.java
+++ src/WriteCommand.java
@@ -1,13 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * WriteCommand Class
+ * 
+ * @author Savannah Noblitt
+ */
+
 public class WriteCommand extends Command {
+    private Scanner in;
     
-    //May need code added, just removing inheritance errors
+    /**
+     * WriteCommand constructor which calls its super class's constructor
+     * 
+     * @param doc the document to write t0
+     */
     public WriteCommand(Document doc)
     {
         super(doc);
     }
 
+    /**
+     * Writes to the document and prints a message to let the user know the action was completed
+     * 
+     * @return the message for writing to the document
+     */
     public String execute()
     {
-        return "";
+        System.out.println("Enter text: ");
+        String line = document.write(in.nextLine());
+        System.out.println();
+        return line;
     }
 }
