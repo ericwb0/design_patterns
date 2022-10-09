38b922a2732a5ad9eabfe55733b34612b06c0487
ericwb0 <burkholdereric03@gmail.com>
2022-10-09 11:59:27 -0400

    First iteration of AppendCommand

--- src/AppendCommand.java
+++ src/AppendCommand.java
@@ -1,14 +1,30 @@
-public class AppendCommand extends Command {
+import java.util.Scanner;
 
-    //May need code added, just removing inheritance errors
+/**
+ * AppendCommand class
+ * @author Eric Burkholder
+ */
+public class AppendCommand extends Command {
+    private Scanner in;
+
+    /**
+     * Constructor for AppendCommand. Calls the Commmand superclass constructor
+     * @param doc the Document that the command will append to
+     */
     public AppendCommand(Document doc)
     {
         super(doc);
     }
 
+    /**
+     * Takes user input and calls Document.append() with the input
+     * @return the result message of the document append operation
+     */
     public String execute()
-    {
-        return "";
+    {   
+        System.out.print("Enter text: ");
+        String ret = document.append(in.nextLine());
+        System.out.println(); // Print \n after user input
+        return ret;
     }
-    
 }
