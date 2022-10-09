d217fca65ae0243a984dd56409a94b4ac9813df6
WyattDCarhart <wdcarhart@icloud.com>
2022-10-09 15:56:31 -0400

    first iteration of InputHandler

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -1,3 +1,38 @@
+import java.util.HashMap;
+
+/**
+ * 
+ * @author wyattcarhart
+ *
+ */
 public class InputHandler {
+	
+	public static final String[] KEYS = {"append","write","view","save"};
     
+	private HashMap<String,Command> commands;
+	
+	/**
+	 * Creates a new InputHandler for the specified document; adds Commands to commands
+	 * @param document the document for the commands to be executed on
+	 */
+	public InputHandler(Document document) {
+		commands = new HashMap<String,Command>();
+		initCommands(document);
+	}
+	
+	private void initCommands(Document document) {
+		commands.put(KEYS[0], new AppendCommand(document));
+		commands.put(KEYS[1], new WriteCommand(document));
+		commands.put(KEYS[2], new ViewCommand(document));
+		commands.put(KEYS[3], new SaveCommand(document));
+	}
+	
+	public void inputEntered(String data) {
+		Command command = commands.get(data.toLowerCase());
+		if(command == null) {
+			System.out.println("Sorry, we don't recognize that command");
+			return;
+		}
+		command.execute();
+	}
 }
